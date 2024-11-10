079eb3857b87bf1ca986fca747d2bacfc4e2bc16
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-11-10 19:55:07 +0100

    add flags

---
+++ wc.go
@@ -0,0 +1,11 @@
+package main
+
+import "flag"
+
+var (
+	line      = flag.Bool("l", false, "Show only line count")
+	word      = flag.Bool("w", false, "Show only word count")
+	character = flag.Bool("c", false, "Show only character count")
+)
+
+func main() {}
