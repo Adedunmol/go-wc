1416c8f782283c6a63689085b2feff9ef29d63cb
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-11-11 09:02:23 +0100

    open file and test

--- wc.go
+++ wc.go
@@ -1,6 +1,12 @@
 package main
 
-import "flag"
+import (
+	"bufio"
+	"flag"
+	"io"
+	"io/fs"
+	"strings"
+)
 
 var (
 	line      = flag.Bool("l", false, "Show only line count")
@@ -9,3 +15,40 @@ var (
 )
 
 func main() {}
+
+type Count struct {
+	FileName string
+}
+
+func NewCountFromFile(fileSystem fs.FS, files []string) []Count {
+	var counts []Count
+
+	for _, file := range files {
+		count := Count{FileName: file}
+		_ = getFile(fileSystem, file, &count)
+		counts = append(counts, count)
+	}
+	return counts
+}
+
+func getFile(fileSystem fs.FS, filePath string, count *Count) error {
+	//path, _ := filepath.Abs(filePath)
+	file, _ := fileSystem.Open(filePath)
+
+	defer file.Close()
+	fileData, _ := io.ReadAll(file)
+	_ = splitLines(string(fileData))
+
+	return nil
+}
+
+func splitLines(line string) []string {
+	var lines []string
+	scanner := bufio.NewScanner(strings.NewReader(line))
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
---
+++ wc_test.go
@@ -0,0 +1,27 @@
+package main_test
+
+import (
+	wc "github.com/Adedunmol/go-wc"
+	"reflect"
+	"testing"
+	"testing/fstest"
+)
+
+func TestWordCount(t *testing.T) {
+	fileSystem := fstest.MapFS{
+		"test.txt": {Data: []byte(string("Some random text"))},
+	}
+	files := []string{"test.txt"}
+
+	counts := wc.NewCountFromFile(fileSystem, files)
+	wanted := wc.Count{FileName: "test.txt"}
+
+	assertCountEqual(t, counts[0], wanted)
+}
+
+func assertCountEqual(t *testing.T, got, want wc.Count) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v, got: %v\n", want, got)
+	}
+}
